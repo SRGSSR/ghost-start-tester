65da067f6c732584d2f49274a734f81b7346d44f
Samuel Défago <defagos@gmail.com>
2021-06-25 18:07:01 +0200

    Log events

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -1,6 +1,7 @@
 import ComScore
 import UIKit
 import UserNotifications
+import os.log
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -11,10 +12,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             builder.secureTransmissionEnabled = true
         }
         
-        if let analyticsConfiguration = SCORAnalytics.configuration() {
-            analyticsConfiguration.addClient(with: configuration)
-            analyticsConfiguration.usagePropertiesAutoUpdateMode = .foregroundAndBackground
+        if let launchOptions = launchOptions {
+            Logger(subsystem: "app", category: "notif").info("--> Options: \(launchOptions, privacy: .public)")
         }
+        else {
+            Logger(subsystem: "app", category: "notif").info("--> No options")
+        }
+        
+        SCORAnalytics.configuration().addClient(with: configuration)
         SCORAnalytics.start()
         
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, _ in
@@ -29,6 +34,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
         print("Device Token: \(token)")
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        Logger(subsystem: "app", category: "notif").info("--> User info: \(userInfo, privacy: .public)")
+        completionHandler(.noData)
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
